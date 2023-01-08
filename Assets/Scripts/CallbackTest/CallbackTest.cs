00a746e67b61cccfa062e45480150a9d3c6ca89f
YCZA <2814350365@qq.com>
2023-01-08 14:22:03 +0800

    尝试第一次提交到远程仓库

--- Assets/Scripts/CallbackTest/CallbackTest.cs
+++ Assets/Scripts/CallbackTest/CallbackTest.cs
@@ -10,4 +10,14 @@ public class CallbackTest : MonoBehaviour
         
     }
 
+    public void DoSomething()
+    {
+        Debug.Log("让小明先去写作业");
+        DoHomework();
+    }
+
+    private void DoHomework()
+    {
+        Debug.Log("小明开始写作业");
+    }
 }
